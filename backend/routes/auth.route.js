de90dfadfdfa8635225e96d2be02e07af7b39933
Anish Dangol <dangol.anish001@gmail.com>
2024-03-01 22:22:39 +0545

    added signup functionality controller

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,4 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+const signup = require("../controllers/auth.controller");
+
+router.post("/sign-up", signup);
+// router.post("/sign-in", signin);
+
 module.exports = router;
