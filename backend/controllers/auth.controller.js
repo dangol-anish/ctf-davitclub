0edad49a117a88d70dbdcb36f35338e15e7117d6
Anish Dangol <dangol.anish001@gmail.com>
2024-03-01 22:46:53 +0545

    added signin controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -2,6 +2,8 @@ const bcryptjs = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const connection = require("../utils/dbConnection");
 
+require("dotenv").config();
+
 const signup = async (req, res) => {
   try {
     const { userName, userEmail, userPassword } = req.body;
@@ -46,4 +48,45 @@ const signup = async (req, res) => {
   }
 };
 
-module.exports = signup;
+const signin = async (req, res) => {
+  try {
+    const { userEmail, userPassword } = req.body;
+
+    const checkExistingEmailQuery = `SELECT user_id, user_email, user_password FROM users WHERE user_email=?`;
+
+    connection.query(
+      checkExistingEmailQuery,
+      [userEmail],
+      async (err, existingUser) => {
+        if (err) {
+          return res
+            .status(500)
+            .json({ error: "You don't have an existing account" });
+        }
+
+        if (existingUser.length > 0) {
+          const validPassword = bcryptjs.compareSync(
+            userPassword,
+            existingUser[0].user_password
+          );
+
+          if (validPassword == true) {
+            const aT = jwt.sign(
+              { id: existingUser[0].id },
+              process.env.ACCESS_TOKEN
+            );
+            const expiryDate = new Date(Date.now() + 8.64e7);
+            res.cookie("aT", aT, { httpOnly: true, expires: expiryDate });
+            res.json({
+              message: "Logged in successfully",
+            });
+          }
+        }
+      }
+    );
+  } catch (error) {
+    res.status(500).json({ error: "Please enter valid credentials." });
+  }
+};
+
+module.exports = { signin, signup };
--- backend/utils/dbConnection.js
+++ backend/utils/dbConnection.js
@@ -1,9 +1,11 @@
 const mysqli = require("mysql");
+require("dotenv").config();
+
 const connection = mysqli.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "ctf_davitclub",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 module.exports = connection;
