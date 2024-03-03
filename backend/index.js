c93322cc4c678cf122fb328f111bdeaaff9bf064
Anish Dangol <dangol.anish001@gmail.com>
2024-03-03 20:39:49 +0545

    changed user table query

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ const userAuth = require("./routes/auth.route.js");
 app.use(express.json());
 
 app.use("/auth", userAuth);
+// app.use("/dashboard");
 
 app.listen(process.env.SERVER_PORT, () => {
   console.log(`Server running on port ${process.env.SERVER_PORT}`);
